bb1c907bf478c0b40a58c53cea2bf35792475314
Sujit Sarkar <snigdho532@gmail.com>
2021-04-17 04:07:07 +0600

    2nd committed

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,15 +1,15 @@
 class UserModel{
   String id;
   String phone;
+  String name;
   String password;
   String nID;
   String fatherName;
   String address;
-  String photoUrl;
   String timeStamp;
   String month;
   String year;
   String day;
-  UserModel({this.id,this.photoUrl,this.address,this.phone,this.password,
+  UserModel({this.id,this.name,this.address,this.phone,this.password,
     this.fatherName,this.nID,this.timeStamp,this.year,this.month,this.day});
 }
